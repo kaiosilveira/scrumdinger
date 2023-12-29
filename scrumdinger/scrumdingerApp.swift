370cca311aeffc98d78dc474c520000e25ced603
Kaio Silveira <silveira.kaio@icloud.com>
2023-12-29 11:21:50 +0000

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Kaio Silveira on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
