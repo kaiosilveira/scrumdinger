6118e5b942c3a51ecea11e4308b0b008243785e5
Kaio Silveira <silveira.kaio@icloud.com>
2023-12-29 14:38:32 +0000

    feat: introduce TrailingIconLabelStyle

---
+++ scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  scrumdinger
+//
+//  Created by Kaio Silveira on 29/12/2023.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle : LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
