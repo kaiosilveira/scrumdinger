68e79c3d934f52ef33e54cbc1185fdb6bbaea7b2
Kaio Silveira <silveira.kaio@icloud.com>
2023-12-29 17:19:55 +0000

    feat: implement ScrumsView

--- scrumdinger/ScrumsView.swift
+++ scrumdinger/ScrumsView.swift
@@ -5,4 +5,21 @@
 //  Created by Kaio Silveira on 29/12/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct ScrumsView : View {
+    let scrums: [DailyScrum]
+
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+struct ScrumsView_Preview : PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
