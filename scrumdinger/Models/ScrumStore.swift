89df4ea9ce060397bd0d14de6433ce03cac367d1
Kaio Silveira <silveira.kaio@icloud.com>
2024-01-21 12:45:50 +0000

    feat: introduce ScrumStore

---
+++ scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,22 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by Kaio Silveira on 21/01/2024.
+//
+
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(
+            for: .documentDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: false
+        )
+        .appendingPathComponent("scrums.data")
+    }
+}
