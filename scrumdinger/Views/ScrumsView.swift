8d4aefe5f7d4c7c1db6be6be3d4bacf92f08f9c7
Kaio Silveira <silveira.kaio@icloud.com>
2024-01-21 13:29:31 +0000

    feat(ScrumsView): add saveAction

--- scrumdinger/Views/ScrumsView.swift
+++ scrumdinger/Views/ScrumsView.swift
@@ -6,7 +6,9 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
     @State private var isPresentingNewScrumView = false
+    let saveAction: ()->Void
 
     var body: some View {
         NavigationStack {
@@ -31,12 +33,15 @@ struct ScrumsView: View {
                     isPresentingNewScrumView: $isPresentingNewScrumView
                 )
             }
+            .onChange(of: scenePhase) { phase in
+                if phase == .inactive { saveAction() }
+            }
         }
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: .constant(DailyScrum.sampleData))
+        ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
     }
 }
