130922d50b9579798a0c00325780f0bd7f9da785
Kaio Silveira <silveira.kaio@icloud.com>
2024-01-21 08:12:11 +0000

    feat: add ThemePicker

---
+++ scrumdinger/ThemePicker.swift
@@ -0,0 +1,26 @@
+//
+//  Picker.swift
+//  scrumdinger
+//
+//  Created by Kaio Silveira on 21/01/2024.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+        .pickerStyle(.navigationLink)
+    }
+}
+
+#Preview {
+    ThemePicker(selection: .constant(.periwinkle))
+}
